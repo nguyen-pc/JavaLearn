9ff3403e2d0f0b4e359aa1e0af6d56c92dd72a46
nguyen <nguyenthcs430@gmail.com>
2024-08-21 08:33:45 +0700

    create table

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,13 +1,19 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,39 +24,56 @@ public class User {
     private String address;
     private String phone;
     private String avatar;
+   
 
-    
+    // roleId, user
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
+
     public String getAvatar() {
         return avatar;
     }
+
     public void setAvatar(String avatar) {
         this.avatar = avatar;
     }
-    // public User(long id, String password, String fullName, String address, String phone) {
-    //     this.id = id;
-    //     this.password = password;
-    //     this.fullName = fullName;
-    //     this.address = address;
-    //     this.phone = phone;
+
+    // public User(long id, String password, String fullName, String address, String
+    // phone) {
+    // this.id = id;
+    // this.password = password;
+    // this.fullName = fullName;
+    // this.address = address;
+    // this.phone = phone;
     // }
     public long getId() {
         return id;
     }
+
     public String getEmail() {
         return email;
     }
+
     public String getPassword() {
         return password;
     }
+
     public String getFullName() {
         return fullName;
     }
+
     public String getAddress() {
         return address;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public void setId(long id) {
         this.id = id;
     }
@@ -58,25 +81,27 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
                 + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
 
-   
-
-    
 }
