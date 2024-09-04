c1d7c0874d7e9183f9a4fff412dfa0c4c30e818e
nguyen <nguyenthcs430@gmail.com>
2024-09-04 19:27:20 +0700

    final

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -3,6 +3,10 @@ package vn.hoidanit.laptopshop.controller.client;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +19,9 @@ import jakarta.servlet.http.HttpSession;
 import vn.hoidanit.laptopshop.domain.Cart;
 import vn.hoidanit.laptopshop.domain.CartDetail;
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.Product_;
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.domain.dto.ProductCriteriaDTO;
 import vn.hoidanit.laptopshop.service.ProductService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -141,6 +147,56 @@ public class ItemController {
         String email = (String) session.getAttribute("email");
         this.productService.handleAddProductToCart(email, id, session, quantity);
         return "redirect:/product/" + id;
+    }
+
+    @GetMapping("/products")
+    public String getProductPage(Model model,
+            ProductCriteriaDTO productCriteriaDTO,
+            HttpServletRequest request) {
+        int page = 1;
+        try {
+            if (productCriteriaDTO.getPage().isPresent()) {
+                // convert from String to int
+                page = Integer.parseInt(productCriteriaDTO.getPage().get());
+            } else {
+                // page = 1
+            }
+        } catch (Exception e) {
+            // page = 1
+            // TODO: handle exception
+        }
+
+        // check sort price
+        Pageable pageable = PageRequest.of(page - 1, 10);
+
+        if (productCriteriaDTO.getSort() != null &&
+                productCriteriaDTO.getSort().isPresent()) {
+            String sort = productCriteriaDTO.getSort().get();
+            if (sort.equals("gia-tang-dan")) {
+                pageable = PageRequest.of(page - 1, 10, Sort.by(Product_.PRICE).ascending());
+            } else if (sort.equals("gia-giam-dan")) {
+                pageable = PageRequest.of(page - 1, 10,
+                        Sort.by(Product_.PRICE).descending());
+            }
+        }
+
+        Page<Product> prs = this.productService.fetchProductsWithSpec(pageable,
+                productCriteriaDTO);
+
+        List<Product> products = prs.getContent().size() > 0 ? prs.getContent()
+                : new ArrayList<Product>();
+
+        String qs = request.getQueryString();
+        if (qs != null && !qs.isBlank()) {
+            // remove page
+            qs = qs.replace("page=" + page, "");
+        }
+
+        model.addAttribute("products", products);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", prs.getTotalPages());
+        model.addAttribute("queryString", qs);
+        return "client/product/show";
     }
 
 }
---
+++ target/generated-sources/annotations/vn/hoidanit/laptopshop/domain/Product_.java
@@ -0,0 +1,80 @@
+package vn.hoidanit.laptopshop.domain;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.metamodel.EntityType;
+import jakarta.persistence.metamodel.SingularAttribute;
+import jakarta.persistence.metamodel.StaticMetamodel;
+
+@StaticMetamodel(Product.class)
+@Generated("org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+public abstract class Product_ {
+
+	
+	/**
+	 * @see vn.hoidanit.laptopshop.domain.Product#image
+	 **/
+	public static volatile SingularAttribute<Product, String> image;
+	
+	/**
+	 * @see vn.hoidanit.laptopshop.domain.Product#detailDesc
+	 **/
+	public static volatile SingularAttribute<Product, String> detailDesc;
+	
+	/**
+	 * @see vn.hoidanit.laptopshop.domain.Product#sold
+	 **/
+	public static volatile SingularAttribute<Product, Long> sold;
+	
+	/**
+	 * @see vn.hoidanit.laptopshop.domain.Product#factory
+	 **/
+	public static volatile SingularAttribute<Product, String> factory;
+	
+	/**
+	 * @see vn.hoidanit.laptopshop.domain.Product#quantity
+	 **/
+	public static volatile SingularAttribute<Product, Long> quantity;
+	
+	/**
+	 * @see vn.hoidanit.laptopshop.domain.Product#price
+	 **/
+	public static volatile SingularAttribute<Product, Double> price;
+	
+	/**
+	 * @see vn.hoidanit.laptopshop.domain.Product#name
+	 **/
+	public static volatile SingularAttribute<Product, String> name;
+	
+	/**
+	 * @see vn.hoidanit.laptopshop.domain.Product#shortDesc
+	 **/
+	public static volatile SingularAttribute<Product, String> shortDesc;
+	
+	/**
+	 * @see vn.hoidanit.laptopshop.domain.Product#id
+	 **/
+	public static volatile SingularAttribute<Product, Long> id;
+	
+	/**
+	 * @see vn.hoidanit.laptopshop.domain.Product
+	 **/
+	public static volatile EntityType<Product> class_;
+	
+	/**
+	 * @see vn.hoidanit.laptopshop.domain.Product#target
+	 **/
+	public static volatile SingularAttribute<Product, String> target;
+
+	public static final String IMAGE = "image";
+	public static final String DETAIL_DESC = "detailDesc";
+	public static final String SOLD = "sold";
+	public static final String FACTORY = "factory";
+	public static final String QUANTITY = "quantity";
+	public static final String PRICE = "price";
+	public static final String NAME = "name";
+	public static final String SHORT_DESC = "shortDesc";
+	public static final String ID = "id";
+	public static final String TARGET = "target";
+
+}
+
